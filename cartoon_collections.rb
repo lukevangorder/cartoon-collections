844873c459f238ee5df4b405b8ddea0aa8f4da01
lukevangorder <lukevangorder@gmail.com>
2021-01-13 16:14:24 -0500

    commit tree 0a129c22e893bd9ffbad38e60fb8aca3c4dfc2b4

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(list)
+  list.each_with_index {|name,place| puts "#{place+1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(list)
+  return list.collect {|element| "#{element.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(list)
+  return list.any? {|words| words.length>4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  foods.each do |food|
+    if cheese_types.include?(food)
+      return food
+    end
+  end
+  return nil
 end
